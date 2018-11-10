c593cd876f07f174a41d307852c5f32ec708b3c4
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-09 20:48:44 -0500

    upload vue

---
+++ scr/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png" />
+    <reader v-bind:flowers="stuffInfo"></reader>
+  </div>
+</template>
+
+<script>
+import Reader from './components/Reader';
+
+export default {
+  name: 'App',
+  components: {
+    Reader
+  },
+  data() {
+    return {
+      stuffInfo: [
+        {
+          title: 'Poem #1',
+          author: 'Rachel B.'
+        },
+        {
+          title: 'Poem #2',
+          author: 'Teri K.'
+        },
+        {
+          title: 'Prose #1',
+          author: 'Amber D.'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
---
+++ scr/components/Reader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>flowers</h1>
+    <div class="content" v-for="flower in flowers">
+      <div class="title">{{ flower.title }}</div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  props: ['flowers']
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: 'Futura';
+  font-size: 32px;
+  color: #474747;
+}
+p {
+  font-family: 'Futura';
+  font-size: 16px;
+  color: #474747;
+}
+</style>
+
