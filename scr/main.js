e5ccae8c1811e77241026f351468e38b3fc2f7cf
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-09 22:09:19 -0500

    use http loader

--- scr/main.js
+++ scr/main.js
@@ -1,8 +1,10 @@
-import App from './App.vue';
+// import App from './App.vue';
 
 new Vue({
   el: '#app',
-  components: { App },
+  components: { 
+    'App': httpVueLoader('App.vue') 
+  },
   template: '<App/>'
 });
 
