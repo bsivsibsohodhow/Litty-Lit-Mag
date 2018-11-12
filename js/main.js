fc935c9722d70b3151badfb72ba84ba68c850df7
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-12 16:03:53 -0500

    add reader plugin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-import Vue from 'vue'
-import App from './App'
+const template1 = '<div class="col"><p>';
+const template2 = '</p></div>';
 
-// Vue.config.productionTip = false
-
-
+$(document).ready(function() {
+  $.get('/issues/winter18.txt', function(data) {
+    var lines = data.split('\n');
+    for (i = 0; i < lines.length; i++) {
+      $('#reader-container').append(template1 + lines[i] + template2);
+    }
+  })
+})
