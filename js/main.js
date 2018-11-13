0b09ff9d19a69aa0b050b27da7608d971e9a6bc3
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-13 12:15:04 -0500

    fix broken link

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const template2 = '">';
 const template3 = '</a></div>';
 
 const issue = 'winter18';
-const path= '/issues/' + issue + '/' + issue + 'html';
+const path= '/issues/' + issue + '/' + issue + '.html';
 
 const htmlTemplate1 = '<!doctype html><html class="no-js" lang=""><head><meta charset="utf-8"><meta http-equiv="x-ua-compatible" content="ie=edge"><title>';
 const htmlTemplate2 = '</title><meta name="description" content=""><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><link rel="manifest" href="site.webmanifest"><link rel="favicon" href="icon.png"><link rel="stylesheet" href="/css/main.css"></head><body><div class="index-container"><div class="whitespace"><div class="row"><h-ws>Lorem Ipsum</h-ws> </div> </div> <div class="row"> <div class="col-1"></div> <div class="col-4"> <img src="/img/heading.gif" alt="heading"> </div> <div class="col" style="text-align: center;"> <a href="about.html">about</a> </div> <div class="col" style="text-align: center;"> <a href="current.html">current</a> </div> <div class="col" style="text-align: center;"> <a href="past.html">past</a> </div> <div class="col" style="text-align: center;"> <a href="submit.html">submit</a> </div> <div class="col" style="text-align: center;"> <a href="contact.html">contact</a> </div> <div class="col-1"></div> </div>';
