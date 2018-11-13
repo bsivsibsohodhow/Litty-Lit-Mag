ff2a010c9f33e7026d4187c54c19f3429667ca1e
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-12 20:16:06 -0500

    browse dir with ajax

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 const template1 = '<div class="col"><p>';
 const template2 = '</p></div>';
 
+/*
 $(document).ready(function() {
   $.get('/issues/winter18.txt', function(data) {
     var lines = data.split('\n');
     for (i = 0; i < lines.length; i++) {
       $('#reader-container').append(template1 + lines[i] + template2);
+    }
+  })
+})
+*/
+
+$(document).ready(function() {
+  $.ajax({
+    url: '/issues/winter18',
+    success: function(data) {
+      $(data).find('a.contains(.txt)').each(function() {
+        var lines = $(this).split('\n');
+          for (i = 0; i < lines.length; i++) {
+            $('#reader-container').append(template1 + lines[i] + template2);
+          }
+      })
     }
   })
 })
