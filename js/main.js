7751df105e1d68a167aecc593cd049bf45c258dc
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-12 20:48:45 -0500

    use a tags

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-const template1 = '<div class="col"><p>';
-const template2 = '</p></div>';
+const template1 = '<div class="col"><a href="';
+const template2 = '">';
+const template3 = '</a></div>';
 
 const issue = 'winter18';
+const path= '/issues/' + issue + '/';
+
+var clickTitle = new Array(23);
 
 $(document).ready(function() {
   $.get('/issues/' + issue + '/' + issue + '.txt', function(data) {
     var lines = data.split('\n');
     for (i = 0; i < lines.length; i++) {
-      $('#reader-container').append(template1 + lines[i] + template2);
+      clickTitle[i] = lines[i];
+      clickTitle[i].replace(/\s\g, '');
+      $('#reader-container').append(template1 + path + clickTitle[i] + '.txt' + template2 + lines[i] + template3);
     }
   })
 })
