d7259a72539ecea2ce331821b3f7e75193e97bae
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-23 20:24:17 -0500

    add hidden button & auto click

--- js/flowers.js
+++ js/flowers.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-  $('#flower-truck').load('/issues/' + issue + '/' + issue + '.html #flower-storage', function(responseTxt, statusTxt, jqXHR) {
-    if (statusTxt == 'success') {
-      alert('flowers successfully loaded!')
-        }
-    if (statusTxt == 'error') {
-      alert('Error: ' + jqXHR.status + ' ' + jqXHR.statusTxt)
-    }
+  $('#hidden-click-btn').click(function() {
+    $('#flower-truck').load('/issues/' + issue + '/' + issue + '.html #flower-storage', function(responseTxt, statusTxt, jqXHR) {
+      if (statusTxt == 'success') {
+        alert('flowers successfully loaded!')
+          }
+      if (statusTxt == 'error') {
+        alert('Error: ' + jqXHR.status + ' ' + jqXHR.statusTxt)
+      }
+    })
   })
+})
+
+$(document).ready(function() {
+  $('#hidden-click-btn').click();
 })
