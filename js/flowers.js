20b572686817c8e96d8a87a861058beebe709493
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-23 20:06:21 -0500

    load flowers

--- js/flowers.js
+++ js/flowers.js
@@ -1,1 +1,12 @@
-$(document).ready
+var issue = 'winter18';
+
+$(document).ready(function() {
+  $('#flower-truck').load('/issues/' + issue + '/' + issue + '.html #flower-storage', function(responseTxt, statusTxt, jqXHR) {
+    if (statusTxt == 'success') {
+      alert('flowers successfully loaded!')
+        }
+    if (statusTxt == 'error') {
+      alert('Error: ' + jqXHR.status + ' ' + jqXHR.statusTxt)
+    }
+  })
+})
