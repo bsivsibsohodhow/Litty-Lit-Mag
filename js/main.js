320e973a6c5e3e42a6af80ae4b5a8be06c56b05f
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-12-05 11:46:57 -0500

    change class selector

--- js/main.js
+++ js/main.js
@@ -356,7 +356,7 @@ $(document).ready(function() {
 })
 
 $(document).ready(function() {
- $('.grid-box a').hover(function(e) {
+ $('.wrapper').hover(function(e) {
           for (var ihh = 0; ihh < 6; ihh++) {
               if (this.href.indexOf('#' + ihh) != -1) {
                 //show flower & florist name
